be0e156a686a08175eaf59d52125f701316247dc
PlayingPro <75310551+PlayingPro@users.noreply.github.com>
2025-07-11 23:57:17 +0100

    Test commit, added player hand UI script.

--- Assets/CharacterSO.cs
+++ Assets/CharacterSO.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CharacterSO", menuName = "Card/Character")]
 public class CharacterSO : CardSO
 {
-    
+    public int health;
+    public int attack;
+    public int defense; 
 }
